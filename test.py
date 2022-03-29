fb7803e0d8bbffd45e9375ef6a5e4a6855ed3453
pizza-d <68813619+pizza-d@users.noreply.github.com>
2022-03-29 10:38:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ def solve(n, m, a):
     remainder = 0
     for i in a:
         remainder = (remainder + i) % m
-    print('case: ' + str(remainder))
+    print('remainder: ' + str(remainder))
 
 solve(5,7,(1,2,3,4,5))
