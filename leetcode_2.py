ec3c94fbaf971a3cfa587beb7e6ab13b689a769b
pizza-d <68813619+pizza-d@users.noreply.github.com>
2022-05-04 14:10:51 +0800

    Update and rename leetcode_2_20220504.py to leetcode_2.py

--- leetcode_2.py
+++ leetcode_2.py
@@ -20,3 +20,13 @@ class Solution:
                 key2 = -1
             if key2 > -1:
                 return [key,key2+key+1]
+            
+### solution 3
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        check = {}
+        for key,val in enumerate(nums):
+            val2 = target - val
+            if val2 in check:
+                return [check[val2],key]
+            check[val] = key
